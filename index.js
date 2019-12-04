1032107fb20a239dfb8750e5af62c75b68789e0a
Kevin Bajzek <kevin.bajzek@gmail.com>
2019-12-03 19:54:46 -0600

    remove redis

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const session = require('express-session');
-const redis = require("redis").createClient();
-const RedisStore = require('connect-redis')(session);
+// const redis = require("redis").createClient();
+// const RedisStore = require('connect-redis')(session);
 const passport = require('passport');
 const util = require('util');
 const SteamStrategy = require('passport-steam').Strategy;
